4eaf9cd2c944c7ce36b7da6e3853a4ee5d641ab8
Artur Duarte <arturduartemoraes@gmail.com>
2023-12-10 21:58:16 -0300

    ♻️ refactor: indent code

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-"use strict"
+'use strict';
 
-const getId = (elemento) => document.getElementById(elemento)
-const img = getId("img")
-const buttons = getId("buttons")
-let indiceDaCor = 0
-let intervalId = null
+const getId = (elemento) => document.getElementById(elemento);
+const img = getId('img');
+const buttons = getId('buttons');
+let indiceDaCor = 0;
+let intervalId = null;
 
 const trocaDeCores = (evento) => {
-    sairAutomatico()
-    ligar[evento.target.id]()
-}
+	sairAutomatico();
+	ligar[evento.target.id]();
+};
 
 const proximoIndice = () => {
-
-    indiceDaCor = indiceDaCor < 2 ? ++indiceDaCor : 0
-}
+	indiceDaCor = indiceDaCor < 2 ? ++indiceDaCor : 0;
+};
 
 const mudancaAutomatica = () => {
-    const cores = ["red", "yellow", "green"]
-    const cor = cores[indiceDaCor]
-    ligar[cor]()
-    proximoIndice()
-}
+	const cores = ['red', 'yellow', 'green'];
+	const cor = cores[indiceDaCor];
+	ligar[cor]();
+	proximoIndice();
+};
 
 const sairAutomatico = () => {
-    clearInterval(intervalId)
-}
-
+	clearInterval(intervalId);
+};
 
 const ligar = {
-    "red": () => img.src = "./img/vermelho.png",
-    "yellow": () => img.src = "./img/amarelo.png",
-    "green": () => img.src = "./img/verde.png",
-    "automatic": () => intervalId = setInterval(mudancaAutomatica, 1000)
-}
+	red: () => (img.src = './img/vermelho.png'),
+	yellow: () => (img.src = './img/amarelo.png'),
+	green: () => (img.src = './img/verde.png'),
+	automatic: () => (intervalId = setInterval(mudancaAutomatica, 1000)),
+};
 
 // Eventos
-buttons.addEventListener("click", trocaDeCores)
+buttons.addEventListener('click', trocaDeCores);
--- script2.js
+++ script2.js
@@ -1,42 +1,42 @@
-"use strict"
+'use strict';
 
-const getId = (elemento) => document.getElementById(elemento)
-const semaforo = getId("img")
-let idInterval
+const getId = (elemento) => document.getElementById(elemento);
+const semaforo = getId('img');
+let idInterval;
 
-const ligarVerde = () => semaforo.src = "img/verde.png"
-const ligarAmarelo = () => semaforo.src = "img/amarelo.png"
-const ligarVermelho = () => semaforo.src = "img/vermelho.png"
-const desligarSemaforo = () => semaforo.src = "img/desligado.png"
+const ligarVerde = () => (semaforo.src = 'img/verde.png');
+const ligarAmarelo = () => (semaforo.src = 'img/amarelo.png');
+const ligarVermelho = () => (semaforo.src = 'img/vermelho.png');
+const desligarSemaforo = () => (semaforo.src = 'img/desligado.png');
 
-const semaforoVerde = () => semaforo.src.includes("verde")
-const semaforoAmarelo = () => semaforo.src.includes("amarelo")
-const semaforoVermelho = () => semaforo.src.includes("vermelho")
+const semaforoVerde = () => semaforo.src.includes('verde');
+const semaforoAmarelo = () => semaforo.src.includes('amarelo');
+const semaforoVermelho = () => semaforo.src.includes('vermelho');
 
 const automatico = () => {
-    if (semaforoVerde()) {
-        ligarAmarelo()
-    } else if (semaforoAmarelo()) {
-        ligarVermelho()
-    } else {
-        ligarVerde()
-    }
-}
+	if (semaforoVerde()) {
+		ligarAmarelo();
+	} else if (semaforoAmarelo()) {
+		ligarVermelho();
+	} else {
+		ligarVerde();
+	}
+};
 
 const funcionar = () => {
-    const buttonAutomatico = getId("automatic")
-    if (buttonAutomatico.textContent == "Automático") {
-        buttonAutomatico.textContent = "Parar"
-        idInterval = setInterval(automatico, 1000)
-    } else {
-        clearInterval(idInterval)
-        buttonAutomatico.textContent = "Automático"
-    }
-}
+	const buttonAutomatico = getId('automatic');
+	if (buttonAutomatico.textContent == 'Automático') {
+		buttonAutomatico.textContent = 'Parar';
+		idInterval = setInterval(automatico, 1000);
+	} else {
+		clearInterval(idInterval);
+		buttonAutomatico.textContent = 'Automático';
+	}
+};
 
 //Eventos
-getId("green").addEventListener("click", ligarVerde)
-getId("yellow").addEventListener("click", ligarAmarelo)
-getId("red").addEventListener("click", ligarVermelho)
-getId("img").addEventListener("dblclick", desligarSemaforo)
-getId("automatic").addEventListener("click", funcionar)
+getId('green').addEventListener('click', ligarVerde);
+getId('yellow').addEventListener('click', ligarAmarelo);
+getId('red').addEventListener('click', ligarVermelho);
+getId('img').addEventListener('dblclick', desligarSemaforo);
+getId('automatic').addEventListener('click', funcionar);
